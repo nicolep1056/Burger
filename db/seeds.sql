dad8959f23aa0927082c45dad98550b970c82378
nicolep1056 <nicolep1056@gmail.com>
2018-11-10 07:24:17 -0600

    works, needs css and readme

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Poblano Picasso Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Eggers Can't Be Cheesers Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chile Relleno-You-Didn't Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Let's Give 'Em Something Shiitake 'Bout Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Foot Feta-ish Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hummus a Tune Burger", false);
