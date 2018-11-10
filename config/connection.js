21b2acd9c9ac8af6d9f350dbab936a4996338801
nicolep1056 <nicolep1056@gmail.com>
2018-11-10 11:15:02 -0600

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "123Repeater",
-  database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+   var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+   else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "123Repeater",
+        database: "burgers_db"
+    
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
